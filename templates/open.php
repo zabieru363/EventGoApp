4351311a9d7dbae6907e89c3f4c0bbf0217b0bab
Javier López <zabierujlc@gmail.com>
2023-05-04 17:33:12 +0200

    Archivo plantilla creado para personalizar el titulo de la página y la ruta css

---
+++ templates/open.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0 minimum-scale=1.0">
+        <meta name="keywords" content="eventos, calendario, tareas">
+        <meta name="author" content="Javier López">
+        <meta name="copyrigth" content="Maestre Calatrava">
+        <meta name="robots" content="index">
+        <title><?php echo $title ?></title>
+
+        <!-- Estilos locales -->
+        <link rel="stylesheet" href="<?php echo $css ?>">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
+        rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+        crossorigin="anonymous">
+    </head>
