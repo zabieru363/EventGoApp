dc5b52f9c1cfd6bec48c3b43bc3e1f0cfb6f06de
Javier López <zabierujlc@gmail.com>
2023-05-08 19:03:02 +0200

    Javascript listo para añadir al select las provincias

--- public/js/getCities.js
+++ public/js/getCities.js
@@ -1,8 +1,18 @@
 "use strict";
 
+const citiesSelect = document.getElementById("cities");
+
 fetch("../../controllers/getCities.php")
 .then(res => res.json()
 .then(data => {
     console.log(data);
+    let counter = 1;
+    for(let i = 0; i < data.length; i++) {
+        const option = document.createElement("option");
+        option.setAttribute("value", counter);
+        option.textContent = data[i];
+        option.appendChild(citiesSelect);
+        counter++;
+    }
 })
 .catch(error => "Algo salió mal " + error));
