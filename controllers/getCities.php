a4bea451dde2077f76e17f55acf92eb27d84f397
Javier López <zabierujlc@gmail.com>
2023-05-08 19:11:22 +0200

    Bug del array de ciudades resuelto

--- controllers/getCities.php
+++ controllers/getCities.php
@@ -4,10 +4,8 @@ require_once("UserController.php");
 $user_controller = new UserController();
 
 if($_SERVER["REQUEST_METHOD"] === "GET")
-{
-    $cities = [];
-    
+{    
     $cities = $user_controller->getCities();
 
-    echo json_encode($array);
+    echo json_encode($cities);
 }
