7d444ade7249e7103e3e7a78603d29bd37a96315
Javier López <zabierujlc@gmail.com>
2023-05-08 19:04:56 +0200

    Intentando corregir bug

--- controllers/getCities.php
+++ controllers/getCities.php
@@ -5,6 +5,8 @@ $user_controller = new UserController();
 
 if($_SERVER["REQUEST_METHOD"] === "GET")
 {
+    $cities = [];
+    
     $cities = $user_controller->getCities();
 
     echo json_encode($array);
