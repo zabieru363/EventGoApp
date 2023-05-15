882a4ad2f2989d1ff6f1ab68a584f851d5b1c845
Javier López <zabierujlc@gmail.com>
2023-05-15 10:13:12 +0200

    Ahora el css de bootstraps e carga antes que los estilos locales

--- templates/open.php
+++ templates/open.php
@@ -9,12 +9,12 @@
         <meta name="copyrigth" content="Maestre Calatrava">
         <meta name="robots" content="index">
         <title><?php echo $title ?></title>
-
-        <!-- Estilos locales -->
-        <link rel="stylesheet" href="<?php echo $css ?>">
-
+        
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
         rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
         crossorigin="anonymous">
+
+        <!-- Estilos locales -->
+        <link rel="stylesheet" href="<?php echo $css ?>">
     </head>
