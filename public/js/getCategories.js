0c3eec9af8393970bc6590951acab2f720e6b631
Javier López <zabierujlc@gmail.com>
2023-05-15 12:15:46 +0200

    Petición get para traer las categorias hecha al archivo categoriesHandler.php

---
+++ public/js/getCategories.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const categoriesDiv = document.getElementsByClassName("categories-container")[0];
+
+fetch("../../controllers/categoriesHandler.php")
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(function(category){
+            const categoryDiv = document.createElement("div");
+            categoryDiv.classList.add("category", "shadow");
+            categoryDiv.textContent = category.name;
+            categoriesDiv.appendChild(categoryDiv);
+        });
+    })
+    .catch((error) => "Algo salió mal " + error);
