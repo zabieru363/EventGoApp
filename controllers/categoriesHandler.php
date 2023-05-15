2421d5723e51b9a2409498809db84be6f0c57869
Javier López <zabierujlc@gmail.com>
2023-05-15 17:48:04 +0200

    Faltaba hacer un echo al json_encode del categoriesHandler.php

--- controllers/categoriesHandler.php
+++ controllers/categoriesHandler.php
@@ -6,5 +6,5 @@ $category_controller = new CategoryController();
 if($_SERVER["REQUEST_METHOD"] === "GET")
 {
     $categories = $category_controller->listCategories();
-    json_encode($categories);
+    echo json_encode($categories);
 }
