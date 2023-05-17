5966564f3fe27669977cb31ddb8ad7816e75b12a
Javier López <zabierujlc@gmail.com>
2023-05-17 10:36:40 +0200

    Contenedores dinámicos seleccionados

---
+++ public/js/profile.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const 
+
+const eventsContainer = document.getElementsByClassName("events-container")[0];
+const editProfileContainer = document.getElementsByClassName("edit-profile-container")[0];
