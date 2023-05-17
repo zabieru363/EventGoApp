27df2bade0fe35ae99ade69961abf176d91df300
Javier López <zabierujlc@gmail.com>
2023-05-17 17:50:42 +0200

    Ahora la vista se actualiza cada vez que el usuario modifica su perfil

--- public/js/profile.js
+++ public/js/profile.js
@@ -14,9 +14,6 @@ const elements = [...form.elements];
 const feedbacks = form.getElementsByClassName("invalid-feedback");
 
 const submitBtn = elements.pop();
-submitBtn.disabled = true;
-submitBtn.style.background = "#609ffd";
-
 const imageImput = elements.pop();
 
 const closeModalBtn = document.getElementsByClassName("return-home-btn")[0];
@@ -152,15 +149,22 @@ form.addEventListener("submit", function(e) {
 
             const modal = new bootstrap.Modal(document.getElementById("editProfileSuccessModal"));
             if(data.updated) {
-                this.reset();
                 modal.show();
 
+                // Actualizamos la vista.
+                userInfoElements[0].src = `../../uploads/${data.Image}`;
+                userInfoElements[1].textContent = data.username;
+                userInfoElements[2].textContent = data.name;
+                userInfoElements[3].textContent = data.email;
+
+                /* Poner los datos del usuario en el formulario para que
+                aparezcan por defecto a la hora de mostrarse. */
+                elements[0].value = data.username;
+                elements[1].value = data.name;
+                elements[2].value = data.email;
+
                 elements.forEach(input => input.classList.remove("is-valid"));
-
-                closeModalBtn.addEventListener("click", () => modal.dispose());
-
-                submitBtn.disabled = true;
-                submitBtn.style.background = "#609ffd";
+                closeModalBtn.addEventListener("click", () => modal.hide());
             }
         })
         .catch(error => "Algo salió mal " + error);
