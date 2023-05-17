0b7f61fd95704ea933f24a5f5e15cc82e0923518
Javier López <zabierujlc@gmail.com>
2023-05-17 11:58:49 +0200

    Formulario de edición validado del perfil del lado del cliente

--- public/js/profile.js
+++ public/js/profile.js
@@ -24,7 +24,83 @@ fetch("../../controllers/userDataHandler.php")
     .then(res => res.json())
     .then(data => {
         userInfoElements[0].src = `../../uploads/${data.Image}`;
+        userInfoElements[1].textContent = data.username;
         userInfoElements[2].textContent = data.name;
         userInfoElements[3].textContent = data.email;
     })
     .catch(error => "Algo salió mal " + error);
+
+// Validación de formulario
+const form = document.forms[0];
+const elements = [...form.elements];
+const feedbacks = form.getElementsByClassName("invalid-feedback");
+
+const submitBtn = elements.pop();
+submitBtn.disabled = true;
+submitBtn.style.background = "#609ffd";
+
+const imageImput = elements.pop();
+
+elements[0].addEventListener("input", function() {
+    if(this.value === "") {
+        this.classList.add("is-invalid");
+        this.classList.remove("is-valid");
+        feedbacks[0].classList.add("d-block");
+        feedbacks[0].textContent = "Este campo es obligatorio";
+    }else{
+        this.classList.add("is-valid");
+        this.classList.remove("is-invalid");
+        feedbacks[0].classList.remove("d-block");
+        feedbacks[0].textContent = "";
+    }
+});
+
+elements[1].addEventListener("input", function() {
+    if(this.value === "") {
+        this.classList.add("is-invalid");
+        this.classList.remove("is-valid");
+        feedbacks[1].classList.add("d-block");
+        feedbacks[1].textContent = "Este campo es obligatorio";
+    }else if(/\d/.test(this.value)) {
+        this.classList.add("is-invalid");
+        this.classList.remove("is-valid");
+        feedbacks[1].classList.add("d-block");
+        feedbacks[1].textContent = "El nombre no puede contener números";
+    }else{
+        this.classList.add("is-valid");
+        this.classList.remove("is-invalid");
+        feedbacks[1].classList.remove("d-block");
+        feedbacks[1].textContent = "";
+    }
+});
+
+elements[2].addEventListener("input", function() {
+    if(this.value === "") {
+        this.classList.add("is-invalid");
+        this.classList.remove("is-valid");
+        feedbacks[2].classList.add("d-block");
+        feedbacks[2].textContent = "Este campo es obligatorio";
+    }else if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.value)) {
+        this.classList.add("is-invalid");
+        this.classList.remove("is-valid");
+        feedbacks[2].classList.add("d-block");
+        feedbacks[2].textContent = "El email no es válido";
+    }else{
+        this.classList.add("is-valid");
+        this.classList.remove("is-invalid");
+        feedbacks[2].classList.remove("d-block");
+        feedbacks[2].textContent = "";
+    }
+});
+
+form.addEventListener("input", function() {
+    const checker = elements.every(input => input.classList.contains("is-valid"));
+
+    if(checker) {
+        submitBtn.disabled = false;
+        submitBtn.style.background = "#0B5ED7";
+    }else{
+        submitBtn.disabled = true;
+        submitBtn.style.background = "#609ffd";
+    }
+});
