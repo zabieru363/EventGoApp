127c5bf5ca5afd1ef16d807cfb8e0a9269f81f64
zabieru363 <zabierujlc@gmail.com>
2023-05-21 12:14:56 +0200

    Ahora el index.php manejará las solicitudes

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
-<?php
-    require_once("controllers/HomeController.php");
-    require_once("lib/router.php");
-    $main_controller = new HomeController();
-
-    $main_controller->index();
-?>
+<?php require_once("lib/router.php"); ?>
