9dd3384d6905f6bf12e4fcf8dc73e255a80b00a3
Javier López <zabierujlc@gmail.com>
2023-05-23 13:50:13 +0200

    Ahora el homecontroller envia a la vista la información del usuario

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -1,8 +1,16 @@
 <?php
 require_once("CategoryController.php");
+require_once("UserController.php");
 
 final class HomeController extends BaseController
 {
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = UserModel::getInstance();
+    }
+
     /**
      * Método que crea la vista principal con las
      * categorias y los eventos.
@@ -11,9 +19,12 @@ final class HomeController extends BaseController
     {
         try{
             $category_controller = new CategoryController();
+            $user_controller = new UserController();
             $categoires = $category_controller->listCategories();
+            $user_data = $user_controller->getUserProfileData($_SESSION["id_user"]);
 
             $data = ["categories" => $categoires];
+            $data = ["user_data" => $user_data];
             $this->render("home/home", $data);
         }catch(Exception $e){
             var_dump($e->getMessage());
