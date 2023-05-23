c89737a071f54a52ea5e6185fd1bcb8680befc7e
Javier López <zabierujlc@gmail.com>
2023-05-23 18:46:23 +0200

    Ahora se envia a la vista del perfil también las ciudades para ponerlas en el select de ciudades

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -1,5 +1,6 @@
 <?php
 require_once("UserController.php");
+require_once("CityController.php");
 
 final class ProfileController extends BaseController
 {
@@ -14,11 +15,16 @@ final class ProfileController extends BaseController
     {
         try {
             $user_controller = new UserController();
+            $city_controller = new CityController();
 
             if(isset($_SESSION["id_user"]))
             {
                 $user_data = $user_controller->getUserProfileData($_SESSION["id_user"]);
-                $this->render("user_profile/profile", ["user_data" => $user_data]);
+                $cities = $city_controller->listCities();
+                $this->render("user_profile/profile", [
+                    "user_data" => $user_data,
+                    "cities" => $cities
+                ]);
             }
         }catch(Exception $e){
             var_dump($e->getMessage());
