21aa8c196f53ae5b1bcbc96daf202ef0d28ea32b
Javier López <zabierujlc@gmail.com>
2023-05-23 11:34:40 +0200

    Controlador creado para cerrar sesión (este no renderiza una vista)

---
+++ controllers/LogoutController.php
@@ -0,0 +1,31 @@
+<?php
+require_once("UserController.php");
+
+final class LogoutController extends BaseController
+{
+    /**
+     * Método que crea la vista para el login
+     */
+    public function index():void
+    {
+        try{
+            $user_controller = new UserController();
+
+            if(isset($_COOKIE["remember_me"]))
+            {
+                setcookie("remember_me", "", time() - 3600);
+                $user_controller->deleteToken($_SESSION["id_user"]);
+            }
+
+            unset($_SESSION["id_user"]);
+            unset($_SESSION["username"]);
+
+            session_destroy();
+
+            // Finalmente se redirecciona al index.php
+            header('Refresh: 2; url=index.php');
+        }catch(Exception $e){
+            var_dump($e->getMessage());
+        }
+    }
+}
