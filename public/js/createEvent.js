f1556aa053d25fc056ad43fc431fea06dbd9df0d
Javier López <zabierujlc@gmail.com>
2023-05-24 17:29:03 +0200

    Validando el resto de campos

--- public/js/createEvent.js
+++ public/js/createEvent.js
@@ -21,7 +21,7 @@ elements[0].addEventListener("input", function() {
         this.classList.remove("is-valid");
         feedbacks[0].classList.add("d-block");
         feedbacks[0].textContent = "Este campo es obligatorio";
-    }else if(!(/^[a-zA-Z0-9\s]+$/.test(this.value))) {
+    }else if(!(/^[a-zA-Z0-9\sñÑ]+$/.test(this.value))) {
         this.classList.add("is-invalid");
         this.classList.remove("is-valid");
         feedbacks[0].classList.add("d-block");
@@ -32,4 +32,86 @@ elements[0].addEventListener("input", function() {
         feedbacks[0].classList.remove("d-block");
         feedbacks[0].textContent = "";
     }
+});
+
+elements[1].addEventListener("input", function() {
+    if(this.value === "") {
+        this.classList.add("is-invalid");
+        this.classList.remove("is-valid");
+        feedbacks[1].classList.add("d-block");
+        feedbacks[1].textContent = "Este campo es obligatorio";
+    }else{
+        this.classList.remove("is-invalid");
+        this.classList.add("is-valid");
+        feedbacks[1].classList.remove("d-block");
+        feedbacks[1].textContent = "";
+    }
+});
+
+elements[3].addEventListener("input", function() {
+    if(this.value === "") {
+        this.classList.add("is-invalid");
+        this.classList.remove("is-valid");
+        feedbacks[3].classList.add("d-block");
+        feedbacks[3].textContent = "Este campo es obligatorio";
+    }else{
+        this.classList.remove("is-invalid");
+        this.classList.add("is-valid");
+        feedbacks[3].classList.remove("d-block");
+        feedbacks[3].textContent = "";
+    }
+});
+
+elements[6].addEventListener("input", function() {
+    if(this.value === "") {
+        this.classList.add("is-invalid");
+        this.classList.remove("is-valid");
+        feedbacks[4].classList.add("d-block");
+        feedbacks[4].textContent = "Este campo es obligatorio";
+    }else{
+        this.classList.remove("is-invalid");
+        this.classList.add("is-valid");
+        feedbacks[4].classList.remove("d-block");
+        feedbacks[4].textContent = "";   
+    }
+});
+
+elements[7].addEventListener("input", function() {
+    if(this.value === "") {
+        this.classList.add("is-invalid");
+        this.classList.remove("is-valid");
+        feedbacks[5].classList.add("d-block");
+        feedbacks[5].textContent = "Este campo es obligatorio";
+    }else{
+        this.classList.remove("is-invalid");
+        this.classList.add("is-valid");
+        feedbacks[5].classList.remove("d-block");
+        feedbacks[5].textContent = "";   
+    }
+});
+
+elements[8].addEventListener("input", function() {
+    const files = [...this.files];
+    const allImages = files.every(file => file.type.startsWith("image/"));
+
+    if(files.length > 3) {
+        this.classList.add("is-invalid");
+        this.classList.remove("is-valid");
+        feedbacks[6].classList.add("d-block");
+        feedbacks[6].textContent = "Solo se pueden subir 3 imagenes";
+    }else if(!allImages) {
+        this.classList.add("is-invalid");
+        this.classList.remove("is-valid");
+        feedbacks[6].classList.add("d-block");
+        feedbacks[6].textContent = "Los archivos deben de ser imagenes";
+    }else{
+        this.classList.remove("is-invalid");
+        this.classList.add("is-valid");
+        feedbacks[5].classList.remove("d-block");
+        feedbacks[5].textContent = "";
+    }
+});
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
 });
