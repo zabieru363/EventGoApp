b3a306ae21da4da4ce42eb6dcdccda8248f88564
Javier López <zabierujlc@gmail.com>
2023-05-25 11:46:53 +0200

    Se ha creado un modal en la vista create_event para ir mostrando las diferentes alertas que haya

--- views/create_event/create_event.php
+++ views/create_event/create_event.php
@@ -1,89 +1,103 @@
 <?php
-    session_start();
-    $title = "Crear evento";
-    require_once("templates/open.php");
+session_start();
+$title = "Crear evento";
+require_once("templates/open.php");
 ?>
-    <body>
-        <?php require_once("templates/header.php"); ?>
 
-        <div class="container shadow p-5 mt-3 mb-3">
-            <h1>Crear evento público</h1>
+<body>
+    <?php require_once("templates/header.php"); ?>
 
-            <!-- Formulario para crear eventos -->
-            <form name="create-event-form" method="POST" novalidate class="needs-validation" action="#" enctype="multipart/form-data">
-                <div class="mb-3">
-                    <label for="event_title" class="form-label">Título del evento</label>
-                    <input type="text" class="form-control" id="event_title" name="event_title" placeholder="Título del evento">
-                    <div class="invalid-feedback"></div>
+    <div class="container shadow p-5 mt-3 mb-3">
+        <h1>Crear evento público</h1>
+
+        <!-- Formulario para crear eventos -->
+        <form name="create-event-form" method="POST" novalidate class="needs-validation" action="#" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label for="event_title" class="form-label">Título del evento</label>
+                <input type="text" class="form-control" id="event_title" name="event_title" placeholder="Título del evento">
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="mb-3">
+                <label for="event_description" class="form-label">Descripción</label>
+                <textarea class="form-control" id="event_description" name="event_description" rows="3" placeholder="Información del evento"></textarea>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="mb-3">
+                <label for="administrator" class="form-label">El evento está organizado por</label>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="adminRadio" id="me" value="me">
+                    <label class="form-check-label" for="me">Yo mismo</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="adminRadio" id="other" value="other">
+                    <label class="form-check-label" for="other">Otra persona</label>
                 </div>
 
-                <div class="mb-3">
-                    <label for="event_description" class="form-label">Descripción</label>
-                    <textarea class="form-control" id="event_description" name="event_description" rows="3" placeholder="Información del evento"></textarea>
-                    <div class="invalid-feedback"></div>
-                </div>
+                <input type="text" class="d-none form-control mt-2" id="administrator_name" name="administrator_name" placeholder="Pedro Garcia">
+                <div class="invalid-feedback"></div>
+            </div>
 
-                <div class="mb-3">
-                    <label for="administrator" class="form-label">El evento está organizado por</label>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="adminRadio" id="me" value="me">
-                        <label class="form-check-label" for="me">Yo mismo</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="adminRadio" id="other" value="other">
-                        <label class="form-check-label" for="other">Otra persona</label>
-                    </div>
+            <div class="mb-3">
+                <label for="locations" class="form-label">Lugar del evento</label>
+                <select class="form-select" id="locations" name="locations">
+                    <option value="">Seleccione una localización</option>
+                </select>
 
-                    <input type="text" class="d-none form-control mt-2" id="administrator_name" name="administrator_name" placeholder="Pedro Garcia">
-                    <div class="invalid-feedback"></div>
-                </div>
+                <div class="invalid-feedback"></div>
+            </div>
 
-                <div class="mb-3">
-                    <label for="locations" class="form-label">Lugar del evento</label>
-                    <select class="form-select" id="locations" name="locations">
-                        <option value="">Seleccione una localización</option>
-                    </select>
-
-                    <div class="invalid-feedback"></div>
-                </div>
-
-                <div class="mb-3">
-                    <div class="row">
-                        <div class="col">
-                            <label for="start_date" class="form-label">Fecha y hora de inicio</label>
-                            <input type="datetime-local" class="form-control" id="start_date" name="start_date">
-                            <div class="invalid-feedback"></div>
-                        </div>
-
-                        <div class="col">
-                            <label for="end_date" class="form-label">Fecha y hora de finalización</label>
-                            <input type="datetime-local" class="form-control" id="end_date" name="end_date">
-                            <div class="invalid-feedback"></div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="mb-3 event-images">
-                    <label for="images" class="form-label">Fotos del evento</label>
-                        <div class="drag-area">
-                            <h2>Arrastra y suelta imagenes (máximo 3 imagenes)</h2>
-                            <span>0</span>
-                            <button>O selecciona tus archivos</button>
-                            <input type="file" class="form-control" id="images" name="images[]" hidden multiple>
-                        </div>
-                        
-                        <div id="preview_files"></div>
+            <div class="mb-3">
+                <div class="row">
+                    <div class="col">
+                        <label for="start_date" class="form-label">Fecha y hora de inicio</label>
+                        <input type="datetime-local" class="form-control" id="start_date" name="start_date">
                         <div class="invalid-feedback"></div>
+                    </div>
+
+                    <div class="col">
+                        <label for="end_date" class="form-label">Fecha y hora de finalización</label>
+                        <input type="datetime-local" class="form-control" id="end_date" name="end_date">
+                        <div class="invalid-feedback"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-3 event-images">
+                <label for="images" class="form-label">Fotos del evento</label>
+                <div class="drag-area">
+                    <h2>Arrastra y suelta imagenes (máximo 3 imagenes)</h2>
+                    <span>0</span>
+                    <button>O selecciona tus archivos</button>
+                    <input type="file" class="form-control" id="images" name="images[]" hidden multiple>
                 </div>
 
-                <button type="submit" class="submit-btn">Crear evento</button>
-            </form>
+                <div id="preview_files"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <button type="submit" class="submit-btn">Crear evento</button>
+        </form>
+    </div>
+
+    <div class="modal fade" id="createEventModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel"> <i class="fa-solid fa-badge-check"></i></h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body"></div>
+            </div>
         </div>
+    </div>
 
-        <!-- JavaScript -->
-        <script src="public/js/createEvent.js"></script>
+    <!-- JavaScript -->
+    <script src="public/js/createEvent.js"></script>
 
-        <!-- Bootstrap y Font Awesome -->
-        <?php require_once("templates/cdns.php"); ?>
-    </body>
+    <!-- Bootstrap y Font Awesome -->
+    <?php require_once("templates/cdns.php"); ?>
+</body>
+
 </html>
