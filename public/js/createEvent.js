017564667a6b91b5b022a73f71242cf55afb9f01
Javier López <zabierujlc@gmail.com>
2023-05-25 22:41:46 +0200

    Intentando validar formulario de creación de eventos

--- public/js/createEvent.js
+++ public/js/createEvent.js
@@ -166,8 +166,8 @@ eventImagesInput.addEventListener("change", function() {
 form.addEventListener("input", function() {
     const fields = [eventTitleInput, eventDescriptionTextarea, eventLocationSelect, eventStartDateInput, eventEndDateInput, eventImagesInput];
     if(adminRadio.value === "other") {
+        fields.push(adminNameInput);
         if(adminNameInput.value === "") {
-            fields.push(adminNameInput);
             submitBtn.disabled = true;
             submitBtn.style.background = "#8dffcc";
         }
