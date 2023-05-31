032c8a94021fb2cb30c93308f4895dcf1f31a1ae
zabieru363 <zabierujlc@gmail.com>
2023-05-31 23:47:35 +0200

    Importando a la vista home un nuevo script de javascript que se encargará del tema de sistema de participación de eventos

--- views/home/home.php
+++ views/home/home.php
@@ -9,6 +9,7 @@
 
         <!-- JavaScript -->
         <script src="public/js/getEventsCategory.js" type="module"></script>
+        <script src="public/js/participationSystem.jsy.js"></script>
 
         <!-- Bootstrap y Font Awesome -->
         <?php require_once("templates/cdns.php"); ?>
