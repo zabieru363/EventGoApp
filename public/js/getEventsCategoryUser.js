da9e8dca1accc7214a143bcfc3fcbdc26666ba12
zabieru363 <zabierujlc@gmail.com>
2023-06-02 00:13:59 +0200

    Fetch preparado para obtener via get todos los eventos a través del php

--- public/js/getEventsCategoryUser.js
+++ public/js/getEventsCategoryUser.js
@@ -3,11 +3,13 @@
 import Event from "./entities/Event.js";
 
 const categories = [...document.getElementsByClassName("category")];
-const firstCategory = categories[0];
-
-randomCategory.classList.add("category-active");
-randomCategory.innerHTML += "<i class='ms-3 fa-regular fa-circle-check'></i>";
-
 const eventsContainer = document.getElementsByClassName("events-container")[0];
 
-// * CARGAMOS LOS EVENTOS DE LA PRIMERA CATEGORÍA
+// * CARGAMOS TODOS LOS EVENTOS
+fetch("controllers/getAllEventsHandler.php")
+    .then(res => res.json())
+    .then(data => {
+        // const firstCategory = categories[0];
+        console.log(data);
+    })
+    .catch(error => console.log("Algo salió mal " + error));
