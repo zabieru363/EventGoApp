db057eeb1c35396f441e891a13b6c799cb43155c
zabieru363 <zabierujlc@gmail.com>
2023-06-02 00:15:05 +0200

    Handler getAllEventsHandler preparado para enviar a javascript todos los eventos

--- controllers/getAllEventsHandler.php
+++ controllers/getAllEventsHandler.php
@@ -2,15 +2,11 @@
 require_once("EventController.php");
 require_once("UserController.php");
 
-if($_SERVER["REQUEST_METHOD"] === "POST")
+if($_SERVER["REQUEST_METHOD"] === "GET")
 {
-    $json = file_get_contents('php://input');
-    $data = json_decode($json);
-
     $event_controller = new EventController();
     $user_controller = new UserController();
     $events = $event_controller->getAllEventsy($_SESSION["id_user"]);
 
-    header("Content-Type: application/json");
     echo json_encode($events);
 }
