79446462e48a07f686b61d9537331a45b7c005cf
zabieru363 <zabierujlc@gmail.com>
2023-06-03 12:47:33 +0200

    Ahora la vista home importa solo un fichero de javascript

--- views/home/home.php
+++ views/home/home.php
@@ -8,11 +8,7 @@
         <?php require_once("templates/eventsZone.php"); ?>
 
         <!-- JavaScript -->
-        <?php if(isset($_SESSION["id_user"])): ?>
-            <script src="public/js/getEventsCategoryUser.js" type="module"></script>
-        <?php else: ?>
-            <script src="public/js/getEventsCategory.js" type="module"></script>
-        <?php endif; ?>
+        <script src="public/js/getEventsCategory.js" type="module"></script>
 
         <!-- Bootstrap y Font Awesome -->
         <?php require_once("templates/cdns.php"); ?>
