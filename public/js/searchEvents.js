b52bb12b3191884a2e3d29b1e95ba9243df5f19d
zabieru363 <zabierujlc@gmail.com>
2023-06-04 13:54:45 +0200

    El script searchEvents ya permite buscar eventos

--- public/js/searchEvents.js
+++ public/js/searchEvents.js
@@ -6,16 +6,25 @@ const resultsDatalist = document.getElementsByClassName("event-results-datalist"
 eventInput.addEventListener("input", getResults);
 
 function getResults() {
-    const formData = new FormData();
-    formData.append("search", this.value);
-
-    fetch("controllers/searchEventsHandler.php", {
-        method: "POST",
-        body: formData
-    })
-        .then(res => res.json())
-        .then(data => {
-
+    if(this.value !== "") {     // Si no hay nada en el formulario no hay necesidad de hacer ninguna petición.
+        const formData = new FormData();
+        formData.append("search", this.value);
+    
+        fetch("controllers/searchEventsHandler.php", {
+            method: "POST",
+            body: formData
         })
-        .catch(error => console.log("Algo salió mal " + error));
+            .then(res => res.json())
+            .then(data => {
+                resultsDatalist.innerHTML = "";
+
+                data.forEach(function(result) {
+                    const option = document.createElement("option");
+                    option.value = result.id;
+                    option.textContent = result.title;
+                    resultsDatalist.appendChild(option);
+                });
+            })
+            .catch(error => console.log("Algo salió mal " + error));
+    }
 }
