e220a5936bdf683677e1fb8c231e391158eccb52
zabieru363 <zabierujlc@gmail.com>
2023-06-05 16:28:39 +0200

    Ahora en el login se controla si el usuario está activo o no

--- controllers/loginHandler.php
+++ controllers/loginHandler.php
@@ -15,21 +15,22 @@ if($_SERVER["REQUEST_METHOD"] === "POST")
     $user = trim($_POST["username"]);
     $password = trim($_POST["pass"]);
 
-    if(isset($_POST["remember-me"]) && $_POST["remember-me"] === "on")
-    {
-        $remember_me = true;
-    }
+    if(isset($_POST["remember-me"]) && $_POST["remember-me"] === "on") $remember_me = true;
 
     $info = $user_controller->login($user, $password, $remember_me);
 
     if($info["login"])
     {
         $login_info["login"] = true;
+        echo json_encode($login_info);
+    }
+    else if(!($info["active"]))
+    {
+        header("Location: index.php?url=userDisabled");
     }
     else
     {
         $login_info["message"] = $info["message"];
+        echo json_encode($login_info);
     }
-
-    echo json_encode($login_info);
 }
