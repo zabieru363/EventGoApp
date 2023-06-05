6aa7f7fd9e4497ec8c10d87206590640e50eaca1
zabieru363 <zabierujlc@gmail.com>
2023-06-05 17:54:17 +0200

    Vista de cuenta suspendida modificada con mejores estilos

--- views/home/home.php
+++ views/home/home.php
@@ -9,7 +9,6 @@
 
         <!-- JavaScript -->
         <script src="public/js/getEventsCategory.js" type="module"></script>
-        <script src="public/js/systemParticipation.js"></script>
         <script src="public/js/searchEvents.js"></script>
 
         <!-- Bootstrap y Font Awesome -->
