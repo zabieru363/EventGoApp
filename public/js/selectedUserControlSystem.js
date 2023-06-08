368aac857709b151b7cc13d299f37c8fd2968f3f
zabieru363 <zabierujlc@gmail.com>
2023-06-08 14:26:16 +0200

    La eliminación y baneo de usuarios funciona correctamente

--- public/js/selectedUserControlSystem.js
+++ public/js/selectedUserControlSystem.js
@@ -8,22 +8,41 @@ const chekboxes = [...document.getElementsByClassName("user-selected")];
 async function sendSelectedUsers(action) {
     const selectedCheckboxes = chekboxes.filter(checkbox => checkbox.checked);
     if(selectedCheckboxes.length) {
-        const selectedIds = selectedCheckboxes.map(checkbox => checkbox.value);
+        const confirmation = confirm("¿Seguro que quiere relizar esta operación?");
+
+        if(confirmation) {
+            const selectedIds = selectedCheckboxes.map(checkbox => checkbox.value);
+        
+            try {
+                const response = await fetch("controllers/userAdminHandler.php", {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({users_selected: selectedIds, action: action})
+                });
+        
+                if(response.ok) {
+                    const data = await response.json();
     
-        try {
-            const response = await fetch("controllers/userAdminHandler.php", {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({users_selected: selectedIds, action: action})
-            });
+                    // Actualizamos la vista
+                    if(action === "delete") {
+                        selectedIds.forEach(function(id) {
+                            const userRow = document.getElementsByClassName(`user-row-${id}`)[0];
+                            userRow.remove();
+                        });
+                    }
     
-            if(response.ok) {
-                const data = await response.json();
+                    if(action === "ban") {
+                        selectedIds.forEach(function(id) {
+                            const userRow = document.getElementsByClassName(`user-row-${id}`)[0];
+                            userRow.children[6].textContent = "NO";
+                        });
+                    }
+                }
+            }catch(error) {
+                console.error("Algo salió mal " + error);
             }
-        }catch(error) {
-            console.error("Algo salió mal " + error);
         }
     }else{
         console.log("No ss ha seleccionado ningún usuario");
