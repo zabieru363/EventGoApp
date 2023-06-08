19935b4328da4bdbe47a75625b7adf79ee92e6d0
zabieru363 <zabierujlc@gmail.com>
2023-06-08 12:59:28 +0200

    Se ha creado una función asíncrona que recibe un parametro que es la acción que vamos a realizar con esos usarios

--- public/js/selectedUserControlSystem.js
+++ public/js/selectedUserControlSystem.js
@@ -3,11 +3,38 @@
 const deleteUsersButton = document.getElementById("delete-selected-users-btn");
 const disabledUsersButton = document.getElementById("disabled-selected-users-btn");
 
-// Recogemos todos los usuarios seleccionados
 const chekboxes = [...document.getElementsByClassName("user-selected")];
 
-deleteUsersButton.addEventListener("click", function() {
+async function sendSelectedUsers(action) {
     const selectedCheckboxes = chekboxes.filter(checkbox => checkbox.checked);
-    const selectedIds = selectedCheckboxes.map(checkbox => checkbox.value);
-    console.log(selectedIds);
+    if(selectedCheckboxes.length) {
+        const selectedIds = selectedCheckboxes.map(checkbox => checkbox.value);
+    
+        try {
+            const response = await fetch("controllers/userAdminHandler.php", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({users_selected: selectedIds, action: action})
+            });
+    
+            if(response.ok) {
+                const data = await response.json();
+    
+            }
+        }catch(error) {
+            console.error("Algo salió mal " + error);
+        }
+    }else{
+        console.log("No ss ha seleccionado ningún usuario");
+    }
+}
+
+deleteUsersButton.addEventListener("click", function() {
+    sendSelectedUsers("delete");
+});
+
+disabledUsersButton.addEventListener("click", function() {
+    sendSelectedUsers("ban");
 });
