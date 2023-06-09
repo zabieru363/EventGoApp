561b12a1b93f1332a74894ca638d704300799871
Javier López <zabierujlc@gmail.com>
2023-06-09 17:28:57 +0200

    Se ha asignado un evento clic a todos los botones de eliminar evento para mandar al php el evento que se quiere eliminar

--- public/js/deleteEvent.js
+++ public/js/deleteEvent.js
@@ -3,18 +3,18 @@
 const deleteEventButtons = [...document.getElementsByClassName("delete-event-btn")];
 
 deleteEventButtons.forEach(function(button) {
-    const eventId = button.getAttribute("data-id");
-    const eventContainer = button.closest(".card");
-    console.log(eventId);
-    console.log(eventContainer);
-
-    // fetch("controllers/deleteEventHandler.php", {
-    //     method: "POST",
-    //     body: JSON.stringify({id: eventId})
-    // })
-    //     .then(res => res.json())
-    //     .then(data => {
-
-    //     })
-    //     .catch(error => console.log("Algo salió mal " + error));
+    button.addEventListener("click", function() {
+        const eventId = button.getAttribute("data-id");
+        const eventContainer = button.closest(".card");
+    
+        fetch("controllers/deleteEventHandler.php", {
+            method: "POST",
+            body: JSON.stringify({id: eventId})
+        })
+            .then(res => res.json())
+            .then(data => {
+    
+            })
+            .catch(error => console.log("Algo salió mal " + error));
+    });
 });
