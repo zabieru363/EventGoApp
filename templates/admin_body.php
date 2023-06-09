a50c6819515879338e2819ba7f21095af1b43ac9
Javier López <zabierujlc@gmail.com>
2023-06-09 18:51:29 +0200

    Se ha incluido una nueva opción en la página de administración que servirá para crear categorías.

--- templates/admin_body.php
+++ templates/admin_body.php
@@ -8,8 +8,12 @@
                 <a href="index.php?url=admin&action=users" class="text-decoration-none text text-black">Administrar usuarios</a>
             </div>
 
-            <div class="p-3 shadow admin-option">
+            <div class="p-3 shadow me-4 admin-option">
                 <a href="index.php?url=admin&action=events" class="text-decoration-none text-black">Administrar eventos</a>
+            </div>
+
+            <div class="p-3 shadow admin-option">
+                <a href="index.php?url=admin&action=categories" class="text-decoration-none text-black">Crear categorias</a>
             </div>
         </div>
     </div>
