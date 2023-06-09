e9b4700afbc22cb1578b47d33a5d14a3a2a713f0
Javier López <zabierujlc@gmail.com>
2023-06-09 19:53:07 +0200

    Formulario de creación de categorias validado del lado del cliente

--- public/js/adminCategoriesControl.js
+++ public/js/adminCategoriesControl.js
@@ -3,7 +3,7 @@
 const categories = [...document.getElementsByClassName("category")];
 const form = document.forms[0];
 const elements = [...form.elements];
-const feedbacks = form.getElementsByClassName("invalid-feedback");
+const feedback = form.getElementsByClassName("invalid-feedback")[0];
 
 const submitBtn = elements.pop();
 submitBtn.disabled = true;
@@ -46,17 +46,17 @@ elements[0].addEventListener("input", function() {
     if(this.value === "") {
         this.classList.remove("is-valid");
         this.classList.add("is-invalid");
-        feedbacks.classList.add("d-block");
-        feedbacks[0].textContent = "El nombre no puede ser vacío.";
+        feedback.classList.add("d-block");
+        feedback.textContent = "El nombre no puede ser vacío.";
     }else if(/\d/.test(this.value)) {
         this.classList.remove("is-valid");
         this.classList.add("is-invalid");
-        feedbacks.classList.add("d-block");
-        feedbacks[0].textContent = "El nombre no puede contener números.";
+        feedback.classList.add("d-block");
+        feedback.textContent = "El nombre no puede contener números.";
     }else{
         this.classList.add("is-valid");
         this.classList.remove("is-invalid");
-        feedbacks.classList.remove("d-block");
-        feedbacks[0].textContent = "";
+        feedback.classList.remove("d-block");
+        feedback.textContent = "";
     }
 });
