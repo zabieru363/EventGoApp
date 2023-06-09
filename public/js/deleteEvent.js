ed26fbbe2da9abe52fa6da01020c685df5a4b516
Javier López <zabierujlc@gmail.com>
2023-06-09 18:16:40 +0200

    El script deleteEvent.js ya funciona pero hace falta hacer un cambio más en el php

--- public/js/deleteEvent.js
+++ public/js/deleteEvent.js
@@ -1,22 +1,37 @@
 "use strict";
 
 const deleteEventButtons = [...document.getElementsByClassName("delete-event-btn")];
+const confirmBtn = document.getElementsByClassName("confirm")[0];
+const cancelBtn = document.getElementsByClassName("cancel")[0];
 
 deleteEventButtons.forEach(function(button) {
     button.addEventListener("click", function() {
-        const eventId = button.getAttribute("data-id");
-        const eventContainer = button.closest(".card");
-    
-        fetch("controllers/deleteEventHandler.php", {
-            method: "POST",
-            body: JSON.stringify({id: eventId})
-        })
-            .then(res => {
-                if(res.ok) {
-                    const modal = new bootstrap.Modal(document.getElementById("eventRemovedModal"));
-                    modal.show();
-                }
+        const confirmationModal = new bootstrap.Modal(document.getElementById("eventRemoveConfirmation"));
+        confirmationModal.show();
+
+        confirmBtn.addEventListener("click", function() {
+            confirmationModal.hide();
+            const eventId = button.getAttribute("data-id");
+            const eventContainer = button.closest(".card");
+
+            const formData = new FormData();
+            formData.append("event_id", eventId);
+        
+            fetch("controllers/deleteEventHandler.php", {
+                method: "POST",
+                body: formData
             })
-            .catch(error => console.log("Algo salió mal " + error));
+                .then(res => {
+                    if(res.ok) {
+                        // Si se ha podido eliminar el evento actualizamos la vista.
+                        eventContainer.remove();
+                        const modal = new bootstrap.Modal(document.getElementById("eventRemovedModal"));
+                        modal.show();
+                    }
+                })
+                .catch(error => console.log("Algo salió mal " + error));
+        });
+
+        cancelBtn.addEventListener("click", () => confirmationModal.hide());
     });
 });
