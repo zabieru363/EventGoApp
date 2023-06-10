4d72c82cac212ec21ca5877044b9d9ee77e5285f
zabieru363 <zabierujlc@gmail.com>
2023-06-10 12:25:57 +0200

    Se han creado dos métodos en el DAOCategories para crear y eliminar categorías

--- models/DAOCategories.php
+++ models/DAOCategories.php
@@ -24,6 +24,25 @@ final class DAOCategories
         return self::$instance;
     }
 
+    /**
+     * Método que hace una consulta a la base de datos y crea una categoría.
+     * @param Category Un objeto de la clase Category.
+     * @return Category Un objeto Category con todos los datos listos. 
+     */
+    public function create(Category $category):Category
+    {
+        $sql = "INSERT INTO category VALUES(NULL, :name)";
+        $id = $this->connection->execute_query_id($sql, [":name", $category->__get("name")]);
+
+        $category->__set("id", $id);
+
+        return $category;
+    }
+
+    /**
+     * Método que hace una consulta para obtener todas las categorías.
+     * @return array Un array de objetos Category con todas las categorías.
+     */
     public function list():array
     {
         $sql = "SELECT * FROM category";
@@ -40,5 +59,18 @@ final class DAOCategories
         }
 
         return $this->data;
+    }
+
+    /**
+     * Método que hace una consulta a la base de datos para borrar
+     * una categoría.
+     * @param int El id de la categoría que se quiere eliminar.
+     */
+    public function delete(int $id):bool
+    {
+        $sql = "DELETE FROM category WHERE Id = :category_id";
+        $status = $this->connection->execute_query($sql, [":category_id" => $id]);
+
+        return $status;
     }
 }
