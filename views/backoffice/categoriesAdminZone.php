0d26db0db29726e258e8a3b328f138cdd9b7f7a7
zabieru363 <zabierujlc@gmail.com>
2023-06-10 12:14:49 +0200

    Se ha creado un modal para mostrar los resultados de las diferentes operaciones con las categorías

--- views/backoffice/categoriesAdminZone.php
+++ views/backoffice/categoriesAdminZone.php
@@ -37,6 +37,19 @@
             </div>
         </div>
 
+        <!-- CATEGORIES MODAL --->
+        <div class="modal fade" id="categoryOperationResult" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="categoryOperationResult">Operación realizada <i class="fa-solid fa-badge-check"></i></h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body"></div>
+                </div>
+            </div>
+        </div>
+
         <!-- JavaScript -->
         <script src="public/js/adminCategoriesControl.js"></script>
 
