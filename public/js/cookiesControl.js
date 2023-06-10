759144ca937c1445158bc01f76df90e5575e77e1
zabieru363 <zabierujlc@gmail.com>
2023-06-10 15:41:00 +0200

    Script que controla el modal de cookies creado

---
+++ public/js/cookiesControl.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const acceptCookiesBtn = document.getElementById("accepCookiesBtn");
+const cookiesModal = new bootstrap.Modal("cookiesModal");
+cookiesModal.show();    // Mostramos el modal por defecto.
+
+acceptCookiesBtn.addEventListener("click", async function() {
+    try {
+        localStorage.setItem("accepted-cookies", true);
+        const formData = new FormData();
+        formData.append("cookies-accepted", true);
+
+        const response = await fetch("controllers/cookiesHandler.php", {
+            method: "POST",
+            body: formData
+        });
+
+        if(response.ok) cookiesModal.hide();
+    }catch(error) {
+        console.error("Algo salió mal " + error);
+    }
+});
