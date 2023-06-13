7cf1c21227085fc24a2303be408c1a7203c49ee8
Javier López <zabierujlc@gmail.com>
2023-06-13 13:30:42 +0200

    Fetch preparado para controlar las opciones de participación que eligió el usuario en la vista de detalles de eventos

--- public/js/eventDetailsControl.js
+++ public/js/eventDetailsControl.js
@@ -8,5 +8,49 @@ if(baseContainer.classList.contains("session-clossed")) {
 }
 
 if(baseContainer.classList.contains("session-active")) {
+    const eventContainer = baseContainer.getElementsByClassName("card")[0];
+    const eventId = +eventContainer.getAttribute("data-id");
+    const dropdown = baseContainer.getElementsByClassName("dropdown")[0];
+
+    if(+dropdown.getAttribute("rule") === 1) {
+        let rule = 0;
+        dropdown.children[1].addEventListener("click", async function(e) {
+            if(e.target.classList.contains("opt2")) rule = 2;
+            if(e.target.classList.contains("opt3")) rule = 3;
+            if(e.target.classList.contains("opt4")) rule = 4;
+
+            try {
+                const response = await fetch("controllers/setParticipationRuleEventHandler", {
+                    method: "POST",
+                    body: JSON.stringify({idEvent: eventId, rule, rule})
+                });
     
+                if(response.ok) {
+                    // const data = await response.json();
+                    
+                    const dropdownHTML = {
+                        rule2 : `<div class='dropdown' rule='2'>
+                            <button class='btn btn-success'>Participaré</button>
+                        </div>`,
+                        rule3 : `<div class='dropdown' rule='3'>
+                            <button class='btn btn-danger'>No participaré</button>
+                        </div>`,
+                        rule4: `<div class="dropdown" rule="4">
+                            <button class="btn btn-warning dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Pendiente de confirmar
+                            </button>
+                            <ul class="dropdown-menu event-participation-options">
+                                <li class="dropdown-item op21">Puedo ir</li>
+                                <li class="dropdown-item opt3">No puedo ir</li>
+                            </ul>
+                        </div>`
+                    };
+        
+                    this.innerHTML = dropdownHTML[`rule${rule}`];
+                }
+            }catch(error) {
+                console.error("Algo salió mal " + error);
+            }
+        });
+    }
 }
