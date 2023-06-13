ee158bde38dfeeade6ffb7cddde4e0fc0df129cc
Javier López <zabierujlc@gmail.com>
2023-06-13 13:52:04 +0200

    Se ha movido el json que se encarga de generar el html del dropdown al principio del script para que esté en el scope global

--- public/js/eventDetailsControl.js
+++ public/js/eventDetailsControl.js
@@ -2,6 +2,24 @@
 
 const baseContainer = document.getElementsByClassName("container")[0];
 
+const dropdownHTML = {
+    rule2 : `<div class='dropdown' rule='2'>
+        <button class='btn btn-success'>Participaré</button>
+    </div>`,
+    rule3 : `<div class='dropdown' rule='3'>
+        <button class='btn btn-danger'>No participaré</button>
+    </div>`,
+    rule4: `<div class="dropdown" rule="4">
+        <button class="btn btn-warning dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Pendiente de confirmar
+        </button>
+        <ul class="dropdown-menu event-participation-options">
+            <li class="dropdown-item op21">Puedo ir</li>
+            <li class="dropdown-item opt3">No puedo ir</li>
+        </ul>
+    </div>`
+};
+
 if(baseContainer.classList.contains("session-clossed")) {
     const dropdown = baseContainer.getElementsByClassName("dropdown")[0];
     dropdown.addEventListener("click", () => window.location.replace("index.php?url=login"));
@@ -9,7 +27,7 @@ if(baseContainer.classList.contains("session-clossed")) {
 
 if(baseContainer.classList.contains("session-active")) {
     const eventContainer = baseContainer.getElementsByClassName("card")[0];
-    const eventId = +eventContainer.getAttribute("data-id");
+    const idEvent = +eventContainer.getAttribute("data-id");
     const dropdown = baseContainer.getElementsByClassName("dropdown")[0];
 
     if(+dropdown.getAttribute("rule") === 1) {
@@ -22,31 +40,13 @@ if(baseContainer.classList.contains("session-active")) {
             try {
                 const response = await fetch("controllers/setParticipationRuleEventHandler", {
                     method: "POST",
-                    body: JSON.stringify({idEvent: eventId, rule, rule})
+                    body: JSON.stringify({idEvent, rule})
                 });
     
                 if(response.ok) {
                     // const data = await response.json();
-                    
-                    const dropdownHTML = {
-                        rule2 : `<div class='dropdown' rule='2'>
-                            <button class='btn btn-success'>Participaré</button>
-                        </div>`,
-                        rule3 : `<div class='dropdown' rule='3'>
-                            <button class='btn btn-danger'>No participaré</button>
-                        </div>`,
-                        rule4: `<div class="dropdown" rule="4">
-                            <button class="btn btn-warning dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Pendiente de confirmar
-                            </button>
-                            <ul class="dropdown-menu event-participation-options">
-                                <li class="dropdown-item op21">Puedo ir</li>
-                                <li class="dropdown-item opt3">No puedo ir</li>
-                            </ul>
-                        </div>`
-                    };
         
-                    this.innerHTML = dropdownHTML[`rule${rule}`];
+                    dropdown.innerHTML = dropdownHTML[`rule${rule}`];
                 }
             }catch(error) {
                 console.error("Algo salió mal " + error);
